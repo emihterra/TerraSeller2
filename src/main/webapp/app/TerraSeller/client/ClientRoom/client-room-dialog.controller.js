de72deb65fd976f5ce2ab922b8fd19c2de817c2c
emihterra <emih@terracorp.ru>
2016-06-15 16:56:05 +0300

    удаление доп элементов при удалении помещения тип элемента диалог подтверждения удаления помещения

--- src/main/webapp/app/TerraSeller/client/ClientRoom/client-room-dialog.controller.js
+++ src/main/webapp/app/TerraSeller/client/ClientRoom/client-room-dialog.controller.js
@@ -10,38 +10,57 @@
     function ClientRoomDialogController ($state, ClientRoom, entity, Principal, terraSellerSettingsService, ClientRoomItem) {
         var vm = this;
 
-        vm.clientRoom = entity;
+        vm.clientRoom = {};
         vm.error = null;
         vm.success = null;
         vm.errorNameExists = null;
         vm.newItemEditing = false;
         vm.clientRoomItems = [];
         vm.itemForDeletion = null;
+        vm.clientRoomItem = {
+            id: null,
+            idClientRoom: '',
+            name: '',
+            item_type: '1',
+            i_width: 0,
+            i_height: 0,
+            i_top: 0
+        };
 
         vm.newItem = newItem;
         vm.cancelItemEditing = cancelItemEditing;
         vm.saveItemEditing = saveItemEditing;
         vm.confirmItemDelete = confirmItemDelete;
         vm.toList = toList;
-
+        vm.getItemTypeStr = getItemTypeStr;
 
         function loadItems() {
-            if (vm.clientRoom.id !== null) {
-                ClientRoomItem.query({id_room: vm.clientRoom.id}, function (result) {
+            if ((vm.clientRoom.id)&&(vm.clientRoom.id !== null)) {
+                ClientRoomItem.query({roomId: vm.clientRoom.id}, function (result) {
                     vm.clientRoomItems = result;
                 });
             }
         };
 
-        loadItems();
-
-        Principal.identity().then(function(account) {
-            terraSellerSettingsService.get(account.login).then(function(settings){
-                if(settings.lastClientCode) {
-                    vm.clientRoom.client = settings.lastClientCode;
-                }
+        function getClientId() {
+            Principal.identity().then(function (account) {
+                terraSellerSettingsService.get(account.login).then(function (settings) {
+                    if (settings.lastClientCode) {
+                        vm.clientRoom.client = settings.lastClientCode;
+                    }
+                });
             });
-        });
+        };
+
+        if(entity.$promise) {
+            entity.$promise.then(function (clientRoom) {
+                vm.clientRoom = clientRoom;
+                getClientId();
+                loadItems();
+            });
+        } else {
+            getClientId();
+        };
 
         var onSaveSuccess = function (result) {
             vm.error = null;
@@ -75,7 +94,7 @@
 
         function saveItemEditing() {
             vm.newItemEditing = false;
-            vm.clientRoomItem.id_client_room = vm.clientRoom.id;
+            vm.clientRoomItem.idClientRoom = vm.clientRoom.id;
             ClientRoomItem.save(vm.clientRoomItem, function() {
                 loadItems();
             });
@@ -89,6 +108,23 @@
 
         function clearItemDeletion() {
             vm.itemForDeletion = null;
+        };
+
+        function getItemTypeStr(itemType){
+            var retStr = "";
+
+            switch(itemType){
+              case 1:
+                  retStr = "Окно";
+                  break;
+              case 2:
+                  retStr = "Дверь";
+                  break;
+                default:
+                  retStr = "";
+            }
+
+            return retStr;
         };
 
     }
